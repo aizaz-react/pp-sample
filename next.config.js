bfed1d6af445f5f82f8c4e6c9b86b03a02afb3ff
aizaz-react <aizaz9790@gmail.com>
2024-05-15 23:17:17 +0500

    resolve output build dir issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
   modularizeImports: {
     '@mui/icons-material/?(((\\w*)?/?)*)': {
       transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}'
